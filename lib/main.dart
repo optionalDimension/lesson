bb33349f29da69b307a9b1660cd2525101d6b07d
Rauan Kussembayev <optionaldimension@gmail.com>
2023-10-17 15:48:09 +0600

    yet another lesson

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/region.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,36 +39,72 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Named Tree"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Container(
-              width: double.maxFinite,
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              child: const Column(
-                children: [Text('Total planted'), Text('127')],
-              ),
-            ),
-          ),
-          const Padding(
-              padding: EdgeInsets.only(left: 16),
-              child: Text('List of my trees')),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Column(
-                  children: [_dataItem(), _dataItem()],
-                )),
+          _body(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: _plantButton(),
           )
         ],
       ),
+    );
+  }
+
+  _plantButton() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 44, left: 16, right: 16),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const Regions();
+          }));
+        },
+        child: Container(
+          width: double.maxFinite,
+          padding: const EdgeInsets.all(16),
+          decoration: const BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          child: const Text(
+            'Plant a tree',
+            style: TextStyle(color: Colors.white, fontSize: 22),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+
+  _body() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Container(
+            width: double.maxFinite,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: const Column(
+              children: [Text('Total planted'), Text('127')],
+            ),
+          ),
+        ),
+        const Padding(
+            padding: EdgeInsets.only(left: 16),
+            child: Text('List of my trees')),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                children: [_dataItem(), _dataItem()],
+              )),
+        )
+      ],
     );
   }
 
---
+++ lib/region.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Regions extends StatefulWidget {
+  const Regions({super.key});
+
+  @override
+  State<Regions> createState() => _RegionsState();
+}
+
+class _RegionsState extends State<Regions> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Region'),
+      ),
+    );
+  }
+}
